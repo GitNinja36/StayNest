07baf502b997a673a36f71fc9b0df179948e41e6
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-10-07 00:04:46 +0530

    useing the mongo ATLAS for storing the data

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ app.listen(port, ()=>{
 });
 
 const mongoose = require('mongoose');
+
+const dbUrl = process.env.ATLASDB_URL;
+
 main()
     .then(()=>{
         console.log("connection is successfull");
     })
     .catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect(dbUrl);
 }
 const Listing = require("./models/listing.js");
 
