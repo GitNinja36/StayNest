037fdfcc7b8f98bb54ac1978346882b657b2bf7b
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-10-06 11:53:24 +0530

    adding the mam popup feature and marker feature

--- public/js/map.js
+++ public/js/map.js
@@ -2,11 +2,13 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
 	
 
 const marker1 = new mapboxgl.Marker()
-.setLngLat(coordinates)
-.addTo(map);
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    .setHTML(`<h6>${listing.location}</h6><p>exact location provided after booking</p>`))
+    .addTo(map);
